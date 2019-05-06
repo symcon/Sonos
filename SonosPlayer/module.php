495981da49d55debd24f1ac760006360856f9da9
Michael Maroszek <maroszek@gmx.net>
2019-05-06 18:51:57 +0200

    initial commit

---
+++ SonosPlayer/module.php
@@ -0,0 +1,25 @@
+<?
+
+	class SonosPlayer extends IPSModule
+	{
+		
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
+
+			//Connect to available splitter or create a new one
+			$this->ConnectParent("{B82CE443-9F35-81F0-9D04-B6323D558CCA}");
+
+			$this->RegisterPropertyString("HouseholdID", "");
+			$this->RegisterPropertyString("PlayerID", "");
+        }    
+        
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+	
+	}
+
+?>
