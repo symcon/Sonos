0122c9b2ac4d6e5f485473caabd89206fa340473
Michael Maroszek <maroszek@gmx.net>
2019-05-11 08:34:46 +0200

    prefer sonos error codes

--- SonosCloud/module.php
+++ SonosCloud/module.php
@@ -181,7 +181,8 @@ class SonosCloud extends IPSModule
         $opts = [
           'http'=> [
             'method' => 'GET',
-            'header' => 'Authorization: Bearer ' . $this->FetchAccessToken() . "\r\n" . 'Content-Type: application/x-www-form-urlencoded' . "\r\n"
+            'header' => 'Authorization: Bearer ' . $this->FetchAccessToken() . "\r\n" . 'Content-Type: application/x-www-form-urlencoded' . "\r\n",
+            "ignore_errors" => true
           ]
         ];
         $context = stream_context_create($opts);
@@ -195,7 +196,8 @@ class SonosCloud extends IPSModule
           'http'=> [
             'method'  => 'POST',
             'header'  => 'Authorization: Bearer ' . $this->FetchAccessToken() . "\r\n" . 'Content-Type: application/json' . "\r\n" . 'Content-Length: ' . strlen($content) . "\r\n",
-            'content' => json_encode($content)
+            'content' => $content,
+            "ignore_errors" => true
           ]
         ];
         $context = stream_context_create($opts);
